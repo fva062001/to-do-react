5cae6ca9ef031656753535d88e3de08660af277d
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2022-09-19 17:09:00 -0400

    Code Refactor

--- src/Components/AddNote/AddNote.jsx
+++ src/Components/AddNote/AddNote.jsx
@@ -1,50 +1,65 @@
-import React, {useState} from 'react';
+import React, {useRef} from 'react';
+import ReactDOM from 'react-dom';
 import styles from './AddNote.module.css';
 import Button from 'react-bootstrap/Button';
 
-function AddNote(props){
+function Backdrop(props){
+    return <div className={styles.backdrop} onClick={props.hideForm}/>;
+}
 
-    const [title,setTitle] = useState('');
-    const [content,setContent] = useState('');
+function Modal(props){
 
-    const titleChangeHandler = (e) => {
-        setTitle(e.target.value);
-    }
+    const noteTitle = useRef();
+    const noteContent = useRef();
 
-    const contentChangeHandler = (e) => {
-        setContent(e.target.value);
-    }
 
     const submitNote = (event) => {
         event.preventDefault();
-        if(content.length > 0){
-        const newNote = {id: Math.floor(Math.random() * 100)
-            ,            title: title, 
-                         content: content, 
-                         active: true};
-        props.addNote(newNote);
-        setContent('');
-        setTitle('');
-    }
-    else{
-        alert("Please don't submit an empty note");
-        setContent('');
-    }
+        const title = noteTitle.current.value;
+        const content = noteContent.current.value;
+        if(content.length > 0)
+        {
+            const newNote = {id: Math.floor(Math.random() * 100),title: title, content: content, active: true};
+            props.addNote(newNote);
+            noteTitle.current.value = "";
+            noteContent.current.value = "";
+        }
+        else
+        {
+            alert("Please don't submit an empty note");
+            noteTitle.current.value = "";
+            noteContent.current.value = "";
+        }
     }
 
+
     return(
-        <div>
-            <div className={styles.backdrop} onClick={props.hideForm}/>
-            <div className={styles.card}>
-                <form onSubmit={submitNote}>
-                    <label htmlFor="title">Note Title</label>
-                    <input value={title} onChange={titleChangeHandler} id="title" type="text" />
-                    <label htmlFor="content">Note Content</label>
-                    <input value={content} onChange={contentChangeHandler} id="content" type="text" />
-                    <Button variant='outline-info' type="submit">Add Note</Button>
-                </form>
-            </div>
+        <div className={styles.card}>
+            <form onSubmit={submitNote}>
+                <label htmlFor="title">Note Title</label>
+                <input ref={noteTitle} id="title" type="text" />
+                <label htmlFor="content">Note Content</label>
+                <input ref={noteContent} id="content" type="text" />
+                <Button variant='outline-info' type="submit">Add Note</Button>
+            </form>
         </div>
+    );
+}
+
+
+function AddNote(props){
+
+
+
+
+
+
+
+    return(
+        <>
+            {ReactDOM.createPortal(<Backdrop hideForm={props.hideForm}/>, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Modal addNote={props.addNote}/>, document.getElementById('modal-root'))}
+        </>
 
     );
 }
